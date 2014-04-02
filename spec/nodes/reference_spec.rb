5497db1f631dd7d5f9e291410abba82c3ac6015e
Andre Medeiros <me@andremedeiros.info>
2014-04-02 13:59:12 +0200

    Test Nodes::Reference#keyword? and Nodes::Reference#constant?

---
+++ spec/nodes/reference_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Sloth::Nodes::Reference do
+  [:keyword, :constant].each do |type|
+    describe "##{ type }?" do
+      it "should return true when a #{ type } is referenced" do
+        ref = described_class.new("SomeRef", type)
+        expect( ref.send(:"#{ type }?") ).to be_true
+      end
+
+      it "should return false when something else is referenced" do
+        ref = described_class.new("SomeRef", :foo)
+        expect( ref.send(:"#{ type }?") ).to be_false
+      end
+    end
+  end
+end
